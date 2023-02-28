b587690873338922724887ed861131dafa3c50e4
mattecasu <mattecasu@gmail.com>
2023-02-28 00:12:53 +0000

    bump libs

--- src/main/java/searcher/web/Controller.java
+++ src/main/java/searcher/web/Controller.java
@@ -29,55 +29,55 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RestController
 public class Controller {
 
-    @Autowired
-    private S3Repository s3Repository;
-    @Autowired
-    private LuceneRepository luceneRepository;
+  @Autowired
+  private S3Repository s3Repository;
+  @Autowired
+  private LuceneRepository luceneRepository;
 
-    private final ObjectMapper objectMapper;
+  private final ObjectMapper objectMapper;
 
-    public Controller() {
-        this.objectMapper = new ObjectMapper();
+  public Controller() {
+    this.objectMapper = new ObjectMapper();
+  }
+
+  @RequestMapping(value = "/index", method = {POST})
+  @ResponseBody
+  public ResponseEntity<String> index(@RequestParam String fileUrl) throws URISyntaxException {
+
+    try {
+      String fileContent = s3Repository.getFileContent(fileUrl);
+      List<Product> products = asList(objectMapper.readValue(fileContent, Product[].class));
+      luceneRepository.indexProducts(products);
+      return new ResponseEntity<>("Indexed file " + fileUrl, OK);
+    } catch (IOException e) {
+      log.error(e.getMessage());
+      return new ResponseEntity<>("The file cannot be parsed: " + fileUrl, BAD_REQUEST);
     }
+  }
 
-    @RequestMapping(value = "/index", method = {POST})
-    @ResponseBody
-    public ResponseEntity<String> index(@RequestParam String fileUrl) throws URISyntaxException {
+  @RequestMapping(value = "/query", method = {POST})
+  @ResponseBody
+  public ResponseEntity<List<Product>> query(@RequestParam String queryString,
+                                             @RequestParam(defaultValue = "10") int limit) {
+    try {
+      List<Product> products = luceneRepository.searchProducts(queryString, limit);
+      if (products.isEmpty()) {
+        String didYouMean = luceneRepository.didYouMean(queryString).orElse("");
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap();
+        headers.put("X-did-you-mean", asList(didYouMean));
+        return new ResponseEntity<>(products, headers, OK);
+      } else {
+        return new ResponseEntity<>(products, OK);
+      }
 
-        try {
-            String fileContent = s3Repository.getFileContent(fileUrl);
-            List<Product> products = asList(objectMapper.readValue(fileContent, Product[].class));
-            luceneRepository.indexProducts(products);
-            return new ResponseEntity<>("Indexed file " + fileUrl, OK);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>("The file cannot be parsed: " + fileUrl, BAD_REQUEST);
-        }
+    } catch (ParseException e) {
+      log.error(e.getMessage());
+      return new ResponseEntity<>(newArrayList(), BAD_REQUEST);
+    } catch (IOException e) {
+      log.error(e.getMessage());
+      return new ResponseEntity<>(newArrayList(), INTERNAL_SERVER_ERROR);
     }
-
-    @RequestMapping(value = "/query", method = {POST})
-    @ResponseBody
-    public ResponseEntity<List<Product>> query(@RequestParam String queryString,
-                                               @RequestParam(defaultValue = "10") int limit) {
-        try {
-            List<Product> products = luceneRepository.searchProducts(queryString, limit);
-            if (products.isEmpty()) {
-                String didYouMean = luceneRepository.didYouMean(queryString).orElse("");
-                MultiValueMap<String, String> headers = new LinkedMultiValueMap();
-                headers.put("X-did-you-mean", asList(didYouMean));
-                return new ResponseEntity<>(products, headers, OK);
-            } else {
-                return new ResponseEntity<>(products, OK);
-            }
-
-        } catch (ParseException e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(newArrayList(), BAD_REQUEST);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(newArrayList(), INTERNAL_SERVER_ERROR);
-        }
-    }
+  }
 
 
 }
